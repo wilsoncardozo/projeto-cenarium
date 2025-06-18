b95882e28102389d8a7c778006fcce1e62a72a0d
Wilson Cardozo <98487506+wilsoncardozo@users.noreply.github.com>
2025-06-18 15:00:31 -0300

    PHPMailer e script enviar.php para formulário de contato

---
+++ enviar.php
@@ -0,0 +1,43 @@
+
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
+require 'PHPMailer/src/SMTP.php';
+
+$nome = $_POST['name'];
+$email = $_POST['email'];
+$mensagem = $_POST['message'];
+
+$mail = new PHPMailer(true);
+
+try {
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contato@residencialcenarium.com.br';
+    $mail->Password = 'R20Cena&22';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    $mail->setFrom('contato@residencialcenarium.com.br', 'Cenarium');
+    $mail->addAddress('contato@residencialcenarium.com.br');
+    $mail->addReplyTo($email, $nome);
+
+    $mail->isHTML(true);
+    $mail->Subject = 'Mensagem - Residencial Cenarium';
+    $mail->Body    = "
+        <strong>Nome:</strong> $nome<br>
+        <strong>Email:</strong> $email<br>
+        <strong>Mensagem:</strong><br>$mensagem
+    ";
+
+    $mail->send();
+    header('Location: contato.html');
+    exit;
+} catch (Exception $e) {
+    echo "Erro ao enviar: {$mail->ErrorInfo}";
+}
+?>
